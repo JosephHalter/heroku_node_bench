c758d0d44c4c9366aacf75fd49bb39ffbc72cad9
Joseph HALTER <joseph@openhood.com>
2011-06-11 14:53:12 +0200

    Try without express

--- web.js
+++ web.js
@@ -1,11 +1,14 @@
-var express = require('express');
+var http = require('http');
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var app = http.createServer(function(request, response){
+  request.addListener('end', function(){
+    response.writeHeader(200, {
+      'Content-Type': 'text/plain'
+    });
+    response.write('Hello world!');
+    response.end();
+  });
 });
-
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
   console.log("Listening on " + port);
